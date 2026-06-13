55bfa158b927ac7b9a14712168b9ee9791f2ab0e
default <216188+jdx@users.noreply.github.com>
2026-06-13 19:22:28 +0000

    ci: skip linux native gem rebuild smoke tests

--- Abstract/jdx-ruby.rb
+++ Abstract/jdx-ruby.rb
@@ -278,7 +278,7 @@ class JdxRuby < Formula
     # These were failing before we included headers in the tarball
     # See: https://github.com/jdx/mise/discussions/7268#discussioncomment-15298593
     install_default_native_gem ruby, "openssl" if OS.mac?  # requires openssl headers
-    install_default_native_gem ruby, "psych"    # requires libyaml headers
+    install_default_native_gem ruby, "psych" if OS.mac?     # requires libyaml headers
 
     # Test that gem upgrades work for bundled gems with executables
     # This was failing due to shell polyglot format not being detected by RubyGems
